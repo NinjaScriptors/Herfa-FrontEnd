13ca89107447c4b934e2080d94e70a8fcf4aca1e
HanaaGhazzi <h.ghazzi@outlook.com>
2021-02-26 14:54:13 +0200

    about us !

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Header from './components/Header'
 import Body from './components/Body'
 import StickyFooter from './components/Footer'
+import AboutUs from './components/About-us'
+import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
+     <BrowserRouter>
    <Header/>
    <Body/>
+                <Switch>
+   <Route path="/about-us" component={AboutUs} >
+ </Route>
+   </Switch>
    <StickyFooter/>
+        </BrowserRouter>
    </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import { NavLink } from 'react-router-dom';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         width: 200,
-        color:"white",
+        color: "white",
         backgroundColor: "transparent",
 
     },
@@ -21,11 +23,11 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: theme.spacing(1),
         flex: 1,
         backgroundColor: "transparent",
-        color:"white"
+        color: "white"
     },
     iconButton: {
         padding: 10,
-        color:"white"
+        color: "white"
     },
     divider: {
         height: 20,
@@ -36,27 +38,32 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = (props) => {
     const classes = useStyles();
-  
-    return (
-            <>
-                <header className="index-header">
-                    <nav>
-                        <Paper component="form" className={classes.root}>
 
-                            <InputBase
-                                className={classes.input}
-                                placeholder="Search about?"
-                                inputProps={{ 'aria-label': 'search google maps' }}
-                            />
-                            <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                                <SearchIcon />
-                            </IconButton>
-                            <Divider className={classes.divider} orientation="vertical" />
-                        
-                        </Paper>
-                        <div className="nav-bar">
-                            <a className="nav-link" href="/">Home</a>
-                            <a className="nav-link" href="/">Our Story</a>
+    return (
+        <>
+           
+                    <header className="index-header">
+                        <nav>
+                            <Paper component="form" className={classes.root}>
+
+                                <InputBase
+                                    className={classes.input}
+                                    placeholder="Search about?"
+                                    inputProps={{ 'aria-label': 'search google maps' }}
+                                />
+                                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                                    <SearchIcon />
+                                </IconButton>
+                                <Divider className={classes.divider} orientation="vertical" />
+
+                            </Paper>
+                            <div className="nav-bar">
+                                <a className="nav-link" href="/">Home</a>
+                            
+                                <NavLink className="nav-link" to='/about-us'>Our Story</NavLink>
+
+                           
+                           
                             <a className="nav-link" href="/">Products</a>
                             <div className="nav-dropdown" id="navDropdown">
                                 <a className="nav-link dropdown-title" href="/">Categories</a>
@@ -72,8 +79,7 @@ const Header = (props) => {
                     </nav>
 
                 </header>
-
-
+                
             </>
 
         )
